ce77b6170828c4456074a301c5bdaacc728de5ab
Yuliana Apaza <yuliana.apaza94@gmail.com>
2016-03-18 13:49:11 -0500

    Fixed order in Page, Section, Paragraph and Bookmarks

--- WebContent/resources/js/controllers/bookmark.js
+++ WebContent/resources/js/controllers/bookmark.js
@@ -5,12 +5,12 @@ angular.module('bookmark', [
 	'ngCookies'
 ])
 
-.constant('INSTANCE_URL', 'http://localhost')
+.constant('INSTANCE_URL', 'http://localhost:8081')
 // Online https://df-bookmarks-test.enterprise.dreamfactory.com
 // Kevin http://localhost
 // Yuliana http://localhost:8081
 
-.constant('APP_API_KEY', '837d4c936a7d41830afd45690d8b2b164535f71b6fd694be6fc947105968b489')
+.constant('APP_API_KEY', 'caf80a2fe1a2edc7425e23b840dee6dbe5e3592c5bcc7e472eda0af308d774fb')
 // Online 24122c4f438ef83fee04c70375209ca1b5062d4d06a45fbeff8d16f3de3aceb8
 // Kevin 837d4c936a7d41830afd45690d8b2b164535f71b6fd694be6fc947105968b489
 // Yuliana caf80a2fe1a2edc7425e23b840dee6dbe5e3592c5bcc7e472eda0af308d774fb
@@ -166,92 +166,52 @@ angular.module('bookmark', [
 			enabled : true
 		};
 		
-		// Get Page data
-		var loadPage = function(idPage, callback) {
-			return Page.query({id: idPage}).$promise.then(function (result) {
-				//console.log('Getting Page data, id: ' + idPage);
-				//console.log(JSON.stringify(result));
-				$scope.page = result;
-				$scope.page.sections = [];
-				
-				// Get Sections data
-				var promiseArr = [];
-		        $scope.page.SECTION_LIST.forEach(function (obj, i) {
-		            var promise = loadSection(obj, function(result){
-		            	$scope.page.sections.push(result);
-		            });
-		            promiseArr.push(promise);
-		        });
-		        
-		        $q.all(promiseArr).then(function(){
-		            //console.log(">>>>>>>>>> Continue page here ...");
-		            return callback();
-		        })
-			});
-		};
-
-		var loadSection = function(idSection, callback) {
-	        return Section.query({id : idSection}).$promise.then(function(result) {
-	        	//console.log('Getting Section data, id: ' + idSection);
-	        	//console.log(JSON.stringify(result));
-	        	var section = result;
-	        	section.paragraphs = [];
-	        	
-				// Get Paragraph data
-				var promiseArr = [];
-		        section.PARAGRAPH_LIST.forEach(function (obj, i) {
-		            var promise = loadParagraph(obj, function(result){
-		            	section.paragraphs.push(result);
-		            });
-		            promiseArr.push(promise);
-		        });
-		        
-		        $q.all(promiseArr).then(function(){
-		        	//console.log(">>>>>>>>>> Continue section here ...");
-		            return callback(section);
-		        })
+		Page.query({id: $scope.idPage}).$promise.then(function (result) {
+			//console.log('Getting Page data, id: ' + idPage);
+			//console.log(JSON.stringify(result));
+			$scope.page = result;
+			$scope.page.sections = [];
+			
+			// Get Sections data
+			var sequence = Promise.resolve();
+	        
+			$scope.page.SECTION_LIST.forEach(function (objPAG, iPAG) {
+	        	sequence = sequence.then(function(){
+	        		return Section.query({id : objPAG}).$promise;
+	            })
+	            .then(function(result){	            	
+	            	$scope.$apply(function(){	
+	            		var section = result;
+	            		section.paragraphs = [];
+	            		
+	            		// Get Paragraphs data
+	            		var sequencePAR = Promise.resolve();
+	            		section.PARAGRAPH_LIST.forEach(function(objPAR, iPAR){
+	            			sequencePAR = sequencePAR.then(function(){
+	            				return Paragraph.query({id: objPAR}).$promise;
+	            			})
+	            			.then(function(resultPAR){
+	            				var paragraph = resultPAR;
+	            				paragraph.bookmarks = [];
+	            				
+	            				// Get Bookmarks data
+	            				var sequenceBKM = Promise.resolve();
+	            				paragraph.BOOKMARK_LIST.forEach(function(objBKM, iBKM){
+	            					sequenceBKM = sequenceBKM.then(function(){
+	            						return Bookmark.query({id: objBKM}).$promise;
+	            					})
+	            					.then(function(resultBKM){
+	            						paragraph.bookmarks.push(resultBKM);
+	            					});
+	            				});
+	            				
+		            			section.paragraphs.push(resultPAR);
+		            		});
+	            		})
+	            		$scope.page.sections.push(section);
+            		});
+	            });
 	        });
-		};
-		
-		var loadParagraph = function(idParagraph, callback) {
-	        return Paragraph.query({id : idParagraph}).$promise.then(function(result) {
-	        	//console.log('Getting Paragraph data, id: ' + idParagraph);
-	        	//console.log(JSON.stringify(result));
-	        	var paragraph = result;
-	        	paragraph.bookmarks = [];
-	        	
-				// Get Bookmark data
-				var promiseArr = [];
-				var sequence;
-				
-		        paragraph.BOOKMARK_LIST.forEach(function (obj, i) {
-		        	sequence = Bookmark.query({id : obj}).$promise;
-		        	sequence = sequence.then(function(result){
-		        		paragraph.bookmarks.push(result);
-		        		promiseArr.push(sequence);
-		            }).catch(function(err){
-		                console.log(':c');
-		            });
-		        });
-		        
-		        Promise.all(promiseArr).then(function(){
-		        	//console.log(">>>>>>>>>> Continue paragraph here ...");
-		            return callback(paragraph);
-		        })
-	        });
-		};
-		/*
-		var loadBookmark = function(idBookmark, callback) {
-	        return Bookmark.query({id : idBookmark}).$promise.then(function(result) {
-	        	//console.log('Getting Bookmark data, id: ' + idBookmark);
-	        	//console.log(">>>>>>>>>> Continue bookmark here ...");
-	        	//console.log(JSON.stringify(result));
-	        	return callback(result);
-	        });
-		};
-		*/
-		loadPage($scope.idPage, function(){
-			//console.log(JSON.stringify($scope.page));
 		});
 
 		$scope.setCurrentAdd = function(idSection, idParagraph, indexSection, indexParagraph){
