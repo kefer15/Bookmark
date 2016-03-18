5f155bcfcb21d90eb7d2d1dd7a147b587efa676d
Yuliana Apaza <yuliana.apaza94@gmail.com>
2016-03-18 01:00:28 -0500

    View: Page, Section, Paragraph, Bookmark

--- WebContent/resources/js/controllers/bookmark.js
+++ WebContent/resources/js/controllers/bookmark.js
@@ -153,16 +153,16 @@ angular.module('bookmark', [
 		$scope.page = {};
 		
 		// Get Page data
-		var loadPage = function(idPage) {
-			console.log('Getting Page data, id: ' + idPage);
-			Page.query({id: idPage}).$promise.then(function (result) {
-				console.log(JSON.stringify(result));
+		var loadPage = function(idPage, callback) {
+			return Page.query({id: idPage}).$promise.then(function (result) {
+				console.log('Getting Page data, id: ' + idPage);
+				//console.log(JSON.stringify(result));
 				$scope.page = result;
 				$scope.page.sections = [];
 				
 				// Get Sections data
 				var promiseArr = [];
-		        $scope.page.section_list.forEach(function (obj, i) {
+		        $scope.page.SECTION_LIST.forEach(function (obj, i) {
 		            var promise = loadSection(obj, function(result){
 		            	$scope.page.sections.push(result);
 		            });
@@ -170,19 +170,69 @@ angular.module('bookmark', [
 		        });
 		        
 		        $q.all(promiseArr).then(function(){
-		            console.log("Continue here ...");
+		            console.log(">>>>>>>>>> Continue page here ...");
+		            return callback();
 		        })
 			});
 		};
 		
 		var loadSection = function(idSection, callback) {
-			console.log('Getting Section data, id: ' + idSection);
 	        return Section.query({id : idSection}).$promise.then(function(result) {
-	        	console.log(JSON.stringify(result));
+	        	console.log('Getting Section data, id: ' + idSection);
+	        	//console.log(JSON.stringify(result));
+	        	var section = result;
+	        	section.paragraphs = [];
+	        	
+				// Get Paragraph data
+				var promiseArr = [];
+		        section.PARAGRAPH_LIST.forEach(function (obj, i) {
+		            var promise = loadParagraph(obj, function(result){
+		            	section.paragraphs.push(result);
+		            });
+		            promiseArr.push(promise);
+		        });
+		        
+		        $q.all(promiseArr).then(function(){
+		        	console.log(">>>>>>>>>> Continue section here ...");
+		            return callback(section);
+		        })
+	        });
+		};
+		
+		var loadParagraph = function(idParagraph, callback) {
+	        return Paragraph.query({id : idParagraph}).$promise.then(function(result) {
+	        	console.log('Getting Paragraph data, id: ' + idParagraph);
+	        	//console.log(JSON.stringify(result));
+	        	var paragraph = result;
+	        	paragraph.bookmarks = [];
+	        	
+				// Get Bookmark data
+				var promiseArr = [];
+		        paragraph.BOOKMARK_LIST.forEach(function (obj, i) {
+		            var promise = loadBookmark(obj, function(result){
+		            	paragraph.bookmarks.push(result);
+		            });
+		            promiseArr.push(promise);
+		        });
+		        
+		        $q.all(promiseArr).then(function(){
+		        	console.log(">>>>>>>>>> Continue paragraph here ...");
+		            return callback(paragraph);
+		        })
+	        });
+		};
+		
+		var loadBookmark = function(idBookmark, callback) {
+	        return Bookmark.query({id : idBookmark}).$promise.then(function(result) {
+	        	console.log('Getting Bookmark data, id: ' + idBookmark);
+	        	console.log(">>>>>>>>>> Continue bookmark here ...");
+	        	//console.log(JSON.stringify(result));
 	        	return callback(result);
 	        });
-		}
+		};
 		
-		loadPage($scope.idPage);
+		loadPage($scope.idPage, function(){
+			console.log(JSON.stringify($scope.page));
+		});
 	}
 ]);
