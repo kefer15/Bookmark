319e445bcbf0ef7ac14b85581460b2139548240c
Yuliana Apaza <yuliana.apaza94@gmail.com>
2016-03-17 23:16:59 -0500

    Factories, load page, sections

--- WebContent/resources/js/controllers/bookmark.js
+++ WebContent/resources/js/controllers/bookmark.js
@@ -5,12 +5,12 @@ angular.module('bookmark', [
 	'ngCookies'
 ])
 
-.constant('INSTANCE_URL', 'https://df-bookmarks-test.enterprise.dreamfactory.com')
+.constant('INSTANCE_URL', 'http://localhost:8081')
 // Online https://df-bookmarks-test.enterprise.dreamfactory.com
 // Kevin http://localhost
 // Yuliana http://localhost:8081
 
-.constant('APP_API_KEY', '24122c4f438ef83fee04c70375209ca1b5062d4d06a45fbeff8d16f3de3aceb8')
+.constant('APP_API_KEY', 'caf80a2fe1a2edc7425e23b840dee6dbe5e3592c5bcc7e472eda0af308d774fb')
 // Online 24122c4f438ef83fee04c70375209ca1b5062d4d06a45fbeff8d16f3de3aceb8
 // Yuliana caf80a2fe1a2edc7425e23b840dee6dbe5e3592c5bcc7e472eda0af308d774fb
 
@@ -59,11 +59,74 @@ angular.module('bookmark', [
 ])
 
 /* FACTORIES*/
+.factory('Page', [
+	'$resource',
+	
+	function ($resource) {
+		return $resource('/api/v2/mongodb/_table/Page/:id', { id: '@id' }, {
+			query: {
+				method: 'GET',
+				isArray: false
+			},
+			create: {
+				method: 'POST'
+			},
+			update: {
+				method: 'PUT'
+			},
+			remove: {
+				method: 'DELETE'
+			}
+		});
+	}
+])
+.factory('Section', [
+	'$resource',
+	
+	function ($resource) {
+		return $resource('/api/v2/mongodb/_table/Section/:id', { id: '@id' }, {
+			query: {
+				method: 'GET',
+				isArray: false
+			},
+			create: {
+				method: 'POST'
+			},
+			update: {
+				method: 'PUT'
+			},
+			remove: {
+				method: 'DELETE'
+			}
+		});
+	}
+])
+.factory('Paragraph', [
+	'$resource',
+	
+	function ($resource) {
+		return $resource('/api/v2/mongodb/_table/Paragraph/:id', { id: '@id' }, {
+			query: {
+				method: 'GET',
+				isArray: false
+			},
+			create: {
+				method: 'POST'
+			},
+			update: {
+				method: 'PUT'
+			},
+			remove: {
+				method: 'DELETE'
+			}
+		});
+	}
+])
 .factory('Bookmark', [
 	'$resource',
 	
 	function ($resource) {
-		return $resource('/api/v2/mongodb/_table/Element/:id', { id: '@id' }, {
+		return $resource('/api/v2/mongodb/_table/Bookmark/:id', { id: '@id' }, {
 			query: {
 				method: 'GET',
 				isArray: false
@@ -83,13 +146,43 @@ angular.module('bookmark', [
 
 /* CONTROLLERS */
 .controller('bookmarkController', [
-	'$scope', '$filter', 'Bookmark',
-	function ($scope, $filter, Bookmark) {
+	'$scope', '$filter', '$q', 'Page', 'Section', 'Paragraph', 'Bookmark',
+	function ($scope, $filter, $q, Page, Section, Paragraph, Bookmark) {
+		//Default Page
+		$scope.idPage = 'PAG001';
+		$scope.page = {};
 		
-		$scope.colLabels = [ 'Pk Id' , 'Page View Id' , 'View Id' , 'Title' , 'Organization Id' , 'Visible Mobile' ,
-		                     'General Model' , 'Last Changed By' , 'Last Changed Date' , 'Owner' , 'Puridiom View Id' ,
-		                     'Page Type' , 'Sub Step' , 'View Group Id' , 'Full Browse' , 'Puridiom Process Id' ,
-		                     'Document Type' , 'Enabled' ];
-		console.log($scope.colLabels);
+		// Get Page data
+		var loadPage = function(idPage) {
+			console.log('Getting Page data, id: ' + idPage);
+			Page.query({id: idPage}).$promise.then(function (result) {
+				console.log(JSON.stringify(result));
+				$scope.page = result;
+				$scope.page.sections = [];
+				
+				// Get Sections data
+				var promiseArr = [];
+		        $scope.page.section_list.forEach(function (obj, i) {
+		            var promise = loadSection(obj, function(result){
+		            	$scope.page.sections.push(result);
+		            });
+		            promiseArr.push(promise);
+		        });
+		        
+		        $q.all(promiseArr).then(function(){
+		            console.log("Continue here ...");
+		        })
+			});
+		};
+		
+		var loadSection = function(idSection, callback) {
+			console.log('Getting Section data, id: ' + idSection);
+	        return Section.query({id : idSection}).$promise.then(function(result) {
+	        	console.log(JSON.stringify(result));
+	        	return callback(result);
+	        });
+		}
+		
+		loadPage($scope.idPage);
 	}
 ]);
